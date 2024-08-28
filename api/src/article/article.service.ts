14a4f29ec1f676ea0a260c693732ecbcd2a1fa07
KacperB <kacpero.barszczewski@gmail.com>
2024-08-28 15:33:46 +0200

    change Date type form mongoose to native

--- api/src/article/article.service.ts
+++ api/src/article/article.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Date, Model } from 'mongoose';
+import { Model } from 'mongoose';
 import { ArticleDocument } from './article.schema';
 
 @Injectable()
