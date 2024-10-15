814ff66362ede892ba950e0e45f58a46c406c81f
KacperB <kacpero.barszczewski@gmail.com>
2024-10-15 15:30:12 +0200

    fix

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,10 +1,7 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UserService } from './user.service';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { Role } from '../auth/enums/role.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
-import { RolesGuard } from 'src/auth/guards/roles.guard';
-import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('user')
 export class UserController {
